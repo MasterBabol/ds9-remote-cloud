66b3e847d971b7669b4c2e91b5c4ea3b300e2e58
Babol <babol@live.co.kr>
2018-10-09 18:22:05 +0900

    Add signal rx/tx supports
    * Also add a technology support skeleton

--- server/api/signal/index.js
+++ server/api/signal/index.js
@@ -3,12 +3,78 @@ import os from 'os';
 
 const router = express.Router();
 
-router.get('*', (req, res) => {
-    res.status(200).send();
+const dbDefaultGlobalAnnouncedSignals = {
+    'global-announced-signals': {}
+};
+
+const addSignals = (dst, src) => {
+    let newDst = Object.assign({}, dst);
+    for (var key of Object.keys(src)) {
+        if (newDst[key] != undefined) {
+            newDst[key] += src[key];
+            if (newDst[key] == 0)
+                delete newDst[key];
+        } else
+            newDst[key] = src[key];
+    }
+    return newDst;
+};
+
+const subtractSignals = (dst, src) => {
+    let newDst = Object.assign({}, dst);
+    for (var key of Object.keys(src)) {
+        if (newDst[key] != undefined) {
+            newDst[key] -= src[key];
+        } else
+            newDst[key] = -src[key];
+    }
+    return newDst;
+};
+
+router.get('/:id', (req, res) => {
+    let db = req.db.read();
+    let leId = req.params.id;
+    let le = db.get('local-estates').find({ id: leId });
+    if (le.value()) {
+        let leSignals = le.defaults({
+            'announced-signals': {}
+        }).get('announced-signals').value();
+        res.status(200).send(leSignals);
+    }
+    else
+        res.status(404).end();
 });
 
-router.post('*', (req, res) => {
-    res.status(200).end();
+router.post('/:id', (req, res) => {
+    let db = req.db.read();
+    let leId = req.params.id;
+    let le = db.get('local-estates').find({ id: leId });
+    let gsigs = db.defaults(dbDefaultGlobalAnnouncedSignals).get('global-announced-signals');
+    if (le.value()) {
+        let leSignalsRaw = le.defaults({
+            'announced-signals': {}
+        }).get('announced-signals');
+        let leSignals = leSignalsRaw.value();
+
+        let newSignals = req.body; // {name: count, ...}
+        le.set('announced-signals', newSignals).write();
+        let subtracted = subtractSignals(newSignals, leSignals);
+        let newGsignals = gsigs.value();
+        let added = addSignals(newGsignals, subtracted);
+        db.set('global-announced-signals', added).write();
+
+        res.status(200).send(newSignals);
+    }
+    else
+        res.status(404).end();
+});
+
+router.get('*', (req, res) => {
+    let db = req.db.read();
+    let leId = req.params.id;
+    let leGlobalSignals =
+        db.defaults(dbDefaultGlobalAnnouncedSignals).get('global-announced-signals');
+    res.status(200).send(leGlobalSignals);
 });
 
 export default router;
--- server/api/technology/index.js
+++ server/api/technology/index.js
@@ -1,14 +1,62 @@
 import express from 'express';
 import os from 'os';
+import _ from 'lodash';
 
 const router = express.Router();
 
-router.get('*', (req, res) => {
-    res.status(200).send();
+const dbDefaultGlobalResearches = {
+    'global-researches': {}
+};
+
+const mergeTechnologies = (dst, src) => {
+    let newDst = _.cloneDeep(dst);
+    for (var key of Object.keys(src)) {
+        if (newDst[key] != undefined) {
+            newDst[key]
+        }
+    }
+};
+
+router.get('/:id', (req, res) => {
+    let db = req.db.read();
+    let leId = req.params.id;
+    let le = db.get('local-estates').find({ id: leId });
+    if (le.value()) {
+        let leResearches = le.defaults({
+            'researches': {}
+        }).get('researches').value();
+        res.status(200).send(leResearches);
+    }
+    else
+        res.status(404).end();
 });
 
-router.post('*', (req, res) => {
-    res.status(200).end();
+router.post('/:id', (req, res) => {
+    let db = req.db.read();
+    let leId = req.params.id;
+    let le = db.get('local-estates').find({ id: leId });
+    let gsigs = db.defaults(dbDefaultGlobalResearches).get('global-researches');
+    if (le.value()) {
+        let leResearchesRaw = le.defaults({
+            'researches': {}
+        }).get('researches');
+        let leResearches = leResearchesRaw.value();
+
+        let newResearches = req.body;
+        le.set('researches', newResearches).write();
+
+        res.status(200).send();
+    }
+    else
+        res.status(404).end();
+});
+
+router.get('*', (req, res) => {
+    let db = req.db.read();
+    let leId = req.params.id;
+    let leGlobalResearches = 
+        db.defaults(dbDefaultGlobalResearches).get('global-researches');
+    res.status(200).send(leGlobalResearches);
 });
 
 export default router;
