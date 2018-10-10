473534143099526e5cb3ce2bed598ec7ba3c68bd
Babol <babol@live.co.kr>
2018-10-10 16:50:46 +0900

    Add nonexact mode for inventory api

--- server/api/inventory/index.js
+++ server/api/inventory/index.js
@@ -22,6 +22,7 @@ router.post('*', (req, res) => {
         let invReq = req.body.items;
         if (invReq)
         {
+            let applies = {};
             let dbmods = [];
             for (var itemName in invReq) {
                 let count = invReq[itemName];
@@ -30,18 +31,27 @@ router.post('*', (req, res) => {
                 let have = globalInv.defaults(defaultItem).get(itemName);
 
                 let newCount = have.value() + count;
-                if (newCount < 0)
-                    throw 404;
-                else if (newCount > 0)
-                    dbmods.push(globalInv.set(itemName, newCount));
-                else
-                    dbmods.push(globalInv.unset(itemName));
+                if (newCount < 0) {
+                    console.log(req.query);
+                    if ('nonexact' in req.query) {
+                        applies[itemName] = -have.value();
+                        dbmods.push(globalInv.unset(itemName));
+                    } else
+                        throw 404;
+                }
+                else {
+                    applies[itemName] = count;
+                    if (newCount > 0)
+                        dbmods.push(globalInv.set(itemName, newCount));
+                    else
+                        dbmods.push(globalInv.unset(itemName));
+                }
             }
 
             for (var dbmod of dbmods)
                 dbmod.write();
 
-            res.status(200).send();
+            res.status(200).send(applies);
         } else
             throw 400;
     } catch (e) {
